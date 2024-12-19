6088517a35565f530ac27e764fed0c3183eae512
lmiles50 <lily.iles2115@gmail.com>
2024-12-19 14:21:36 -0500

    all files full, not run yet

--- Desktop/website/script.js
+++ Desktop/website/script.js
@@ -1,1 +1,7 @@
-ECHO is on.
+// script.js
+
+// Function to navigate to different pages
+function goToPage(page) {
+    // Redirect the browser to the specified page
+    window.location.href = page;
+}
